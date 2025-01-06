5dcbcb218af654a361ffef55ff0f14c801a3face
AndhikaW <andhikaw61@gmail.com>
2025-01-07 00:59:45 +0700

    [add] database

--- godongbackend/database/migrations/2024_08_01_032001_create_personal_access_tokens_table.php
+++ godongbackend/database/migrations/2024_08_01_032001_create_personal_access_tokens_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        // Schema::create('personal_access_tokens', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
     }
 
     /**
